a682f605da250391095a9e14f447b138c06cbd94
Hardik Manchanda <117676964+Hunny96@users.noreply.github.com>
2023-01-15 23:37:51 +0530

    Add files via upload

---
+++ 027.Py_prog_assign_2.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 1. Write a Python program to convert kilometers to miles?
+
+# In[1]:
+
+
+km = int(input("Enter the value in kilometers: "))
+
+# 1 Kilometre = 0.621371 Mile
+r = 0.621371
+
+# Converting km to miles.
+miles = km * r
+
+print("The entered value in Miles: ", miles)
+
+
+# ### 2. Write a Python program to convert Celsius to Fahrenheit?
+
+# In[2]:
+
+
+c = int(input("Enter temp in celcius: ")) 
+ 
+# Converting the temperature to fehrenheit 
+f = (c * 1.8) + 32
+
+print("Temp in fehrenhite is:",f)
+
+
+# ### 3. Write a Python program to display calendar?
+
+# In[3]:
+
+
+import calendar
+ 
+yy = 2022
+print(calendar.calendar(yy))
+
+
+# # for given month in a year
+
+# In[4]:
+
+
+import calendar
+ 
+yy = 2023
+mm = 1
+ 
+print(calendar.month(yy, mm))
+
+
+# ### 4. Write a Python program to solve quadratic equation?
+
+# In[7]:
+
+
+# import complex math module  
+import cmath  
+a = float(input('Enter a: '))  
+b = float(input('Enter b: '))  
+c = float(input('Enter c: '))  
+  
+# calculate the discriminant  
+d = (b**2) - (4*a*c)  
+  
+# find two solutions  
+sol1 = (-b-cmath.sqrt(d))/(2*a)  
+sol2 = (-b+cmath.sqrt(d))/(2*a)  
+print('The solution are {0} and {1}'.format(sol1,sol2))   
+
+
+# ### 5. Write a Python program to swap two variables without temp variable?
+
+# In[6]:
+
+
+x = 54
+y = 75
+ 
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+ 
+x, y = y, x
+ 
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
+
+
+# In[ ]:
+
+
+
+
