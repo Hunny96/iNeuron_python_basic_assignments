4a06eae3b933d60663948a2149c2b9171e5cf2fe
Hardik Manchanda <117676964+Hunny96@users.noreply.github.com>
2023-01-19 13:47:01 +0530

    Add files via upload

---
+++ 028.Py_prog_assign_3.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. Write a Python Program to Check if a Number is Positive, Negative or Zero?
+
+# In[3]:
+
+
+a = int(input("Enter a: "))
+if a > 0:
+    print("Positive")
+elif a == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+
+# # 2. Write a Python Program to Check if a Number is Odd or Even?
+
+# In[5]:
+
+
+a = int(input("Enter a: "))
+if a % 2 == 0:
+    print("Even")
+else:
+    print("odd")
+
+
+# # 3. Write a Python Program to Check Leap Year?
+
+# In[8]:
+
+
+year = int(input("Enter year: "))
+if (year % 400 == 0) and (year % 100 == 0):
+    print("Century leap year")
+elif (year % 4 == 0) and (year % 100 != 0):
+    print("Leap year")
+else:
+    print("Not a leap year")
+
+
+# # 4. Write a Python Program to Check Prime Number?
+
+# In[17]:
+
+
+a = int(input("Enter a: "))
+if a == 1:
+    print("prime no")
+    for i in range(2,a):
+        if a % i == 0:
+            print("Not a prime no")
+            break
+    else:
+        print("prime no")
+
+
+# # 5. Write a Python Program to Print all Prime Numbers in an Interval of 1-10000?
+
+# In[18]:
+
+
+lower = 1
+upper = 10000
+
+print("Numbers between", lower ,"and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+
+
+# In[ ]:
+
+
+
+
