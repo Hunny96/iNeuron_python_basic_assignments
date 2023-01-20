bd5fafb00b8cc8168faf8d8401fd900c07afa482
Hardik Manchanda <117676964+Hunny96@users.noreply.github.com>
2023-01-20 14:04:09 +0530

    Add files via upload

---
+++ 029.Py_prog_assign_4.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. Write a Python Program to Find the Factorial of a Number?
+
+# In[1]:
+
+
+num = int(input("Enter number: "))
+fact = 1
+if num == 0:
+    print("Facorial of 0 is 1")
+elif num < 0:
+    print("Factorial doesn't exist for number less than 0")
+else:
+    for i in range(1, num+1):
+        fact *= i
+    print("Factorial of", num, "is", fact)
+
+
+# # 2. Write a Python Program to Display the multiplication Table?
+
+# In[4]:
+
+
+num = int(input("enter number: "))
+print("Multiplication table of", num, "is:")
+for i in range(1,11):
+    print(num, "X", i, "=", num*i)
+
+
+# # 3. Write a Python Program to Print the Fibonacci sequence?
+
+# In[17]:
+
+
+def fib1(n):
+    a = 0
+    b = 1
+    output = []
+    for i in range(n):
+        output.append(a)
+        a, b = b, a+b
+    return output
+fib1(10)
+
+
+# # 4. Write a Python Program to Check Armstrong Number?
+
+# In[19]:
+
+
+num = int(input("Enter a Number:"))
+order = len(str(num))
+temp = num;
+sum = 0
+while(temp>0):
+    digit =temp%10
+    sum += digit **order
+    temp = temp//10
+if(sum==num):
+    print("",num,"is an Armstrong number")
+else:
+    print("",num,"is not an Armstrong number")
+
+
+# # 5. Write a Python Program to Find Armstrong Number in an Interval?
+
+# In[20]:
+
+
+lower = int(input("Enter lower range: "))  
+upper = int(input("Enter upper range: "))  
+  
+for num in range(lower,upper + 1):  
+   sum = 0  
+   temp = num  
+   while temp > 0:  
+       digit = temp % 10  
+       sum += digit ** 3  
+       temp //= 10  
+       if num == sum:  
+            print(num)  
+
+
+# # 6. Write a Python Program to Find the Sum of Natural Numbers?
+
+# In[26]:
+
+
+n=int(input("Enter a number: "))
+sum1 = 0
+while(n > 0):
+    sum1=sum1+n
+    n=n-1
+print("The sum of first n natural numbers is",sum1)
+
+
+# In[ ]:
+
+
+
+
